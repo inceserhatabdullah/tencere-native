6bf0b9c7fa4512903a23bebcc4bf0eb80240be53
inceserhatabdullah <inceserhatabdullah@gmail.com>
2026-01-02 18:50:27 +0300

    feat: Configure Prettier for code formatting and enhance ESLint with TypeScript support.

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,14 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const tsParser = require('@typescript-eslint/parser');
 
 module.exports = defineConfig([
   expoConfig,
   {
+    languageOptions: {
+      parser: tsParser,
+    },
     ignores: ['dist/*'],
   },
 ]);
