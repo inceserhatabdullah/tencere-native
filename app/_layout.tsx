14ea3bc0df9cf1eae86e665548881c80dc7bc81e
inceserhatabdullah <inceserhatabdullah@gmail.com>
2026-01-03 16:47:17 +0300

    intro screen and layout index screen added for splash when the app init first

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
-import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack } from 'expo-router';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded] = useFonts({
+    'google-sans': require('@/assets/fonts/google-sans.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  return <Stack screenOptions={{ headerShown: false }}></Stack>;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,6 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { Redirect } from 'expo-router';
 
-export default function ChatScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tencere</Text>
-      <Text>Malzemelerini yaz, ben tarif önereyim.</Text>
-    </View>
-  );
+export default function IndexScreen() {
+ 
+  return <Redirect href='/intro'/>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: '600',
-    marginBottom: 12,
-  },
-});
